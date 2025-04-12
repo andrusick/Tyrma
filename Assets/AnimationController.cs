39ea72ab0a1a8047b87142aa2b29b48963596403
andrusick <andreyoo476@gmail.com>
2025-04-12 20:55:12 +0700

    аниматор контроллер

--- Assets/AnimationController.cs
+++ Assets/AnimationController.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class AnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    protected Animator animator;
+    protected float horizontalInput;
+    protected float verticalInput;
+        // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("horizontalMoving", horizontalInput);
+        animator.SetFloat("horizontalMoving", verticalInput);
     }
 }
